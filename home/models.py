e25054bf9e26c112d6c8487a4945eb87bbf09bcd
punamthapa <punam.chhetri.37@.com>
2021-03-23 20:55:03 +0545

    23march

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+CATEGORY = (('vector', 'vector'), ('raster', 'raster'), ('ui', 'ui'), ('printing', 'printing'))
 
 # Create your models here.
--- home/views.py
+++ home/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'index.html')
+    views = {}
+    views['feedbacks'] = Review.objects.all()
+    views = {}
+    views["projects"] = Project.objects.all()
+
+
+    return render(request, 'index.html', views)
+
+def about(request):
+    views = {}
+    views['feedbacks'] = Review.objects.all()
+    return render(request, 'about.html', views)
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def price(request):
+    return render(request, 'price.html')
+
+def elements(request):
+    return render(request, 'elements.html')
+
+def blogsingle(request):
+    return render(request, 'blog-single.html')
+
+def bloghome(request):
+    return render(request, 'blog-home.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Contact.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message
+        )
+        data.save()
+    return render(request, 'contact.html')
